70ff53423966f273ca84c26d0f2838ac7867f46c
Rokon Ron <rokon.raz@gmail.com>
2023-08-19 19:39:01 +0600

    Import (update redux) from @reduxjs/toolkit

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,24 +1,38 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { combineWithDevTools, composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer, productDetailsReducer } from './reducers/productReducers'
+// import { createStore, combineReducers, applyMiddleware } from 'redux'
+// import thunk from 'redux-thunk'
+// import { combineWithDevTools, composeWithDevTools } from 'redux-devtools-extension'
+// import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 
 
-const reducer = combineReducers({
+// const reducer = combineReducers({
+//     productList: productListReducer,
+//     productDetails: productDetailsReducer,
+// })
+
+// const initialState = {}
+
+// const middleware = [thunk]
+
+
+// const store = createStore(reducer, initialState, 
+//     composeWithDevTools(applyMiddleware(...middleware)))
+
+
+
+// export default store
+
+
+import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import { productListReducer, productDetailsReducer } from './reducers/productReducers';
+
+const store = configureStore({
+  reducer: {
     productList: productListReducer,
     productDetails: productDetailsReducer,
-})
+  },
+  middleware: [thunk],
+  devTools: process.env.NODE_ENV !== 'production', // Enable devtools only in non-production environment
+});
 
-const initialState = {}
-
-const middleware = [thunk]
-
-
-const store = createStore(reducer, initialState, 
-    composeWithDevTools(applyMiddleware(...middleware)))
-
-
-
-export default store
-
-
+export default store;
