c72461a9b95913e9f35b0f626570e67cf2c6d69d
Rokon Ron <rokon.raz@gmail.com>
2023-08-20 22:07:42 +0600

    create CartScreen.js file and difain a function name cartScreen.

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+// import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import {Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import { Message } from '../components/Message'
+import { addToCart } from '../actions/cartActions'
+import { useLocation, Link, useParams } from 'react-router-dom';
 
-const 
-CartScreen = () => {
+
+// function CartScreen ({match, Location, history}) {
+//   const { id } = useParams();
+//   const productID = id
+//   // const qty = new URLSearchParams(location.search).get('qty'); 
+//   const qty = Location.search
+//   console.log('qty:', qty)
+
+function CartScreen({ match }) {
+  const { id } = useParams();
+  const productID = id;
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const qty = searchParams.get('qty');
+
+  // console.log('qty:', qty);
+
+  
+
   return (
     <div>
         CartScreen
@@ -10,3 +33,6 @@ CartScreen = () => {
 }
 
 export default CartScreen
+
+
+
