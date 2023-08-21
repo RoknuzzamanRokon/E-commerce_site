9ae87129edc8d8d5ffb797518c7b20315faca9c0
Rokon Ron <rokon.raz@gmail.com>
2023-08-21 20:06:39 +0600

    Update cartReducer.js file.import CART_REMOVE_ITEM globel variable in this folder.

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from '../constants/cartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 
 
 const initialState = {
@@ -25,6 +25,11 @@ export const cartReducer = (state = initialState, action) => {
                 }
 
             }
+            case CART_REMOVE_ITEM:
+                return{
+                    ...state,
+                    cartItems:state.cartItems.filter(x => x.product !== action.payload)
+                }
             default:
             return state;
     }
