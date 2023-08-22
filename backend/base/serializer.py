e5248de7dd291f4c4199663c15aeeb7960534d29
Rokon Ron <rokon.raz@gmail.com>
2023-08-23 02:02:12 +0600

    Update serializer file.02

--- backend/base/serializer.py
+++ backend/base/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -11,9 +12,18 @@ class ProductSerializer(serializers.ModelSerializer):
         
 class UserSerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField(read_only = True)
+    _id = serializers.SerializerMethodField(read_only = True)
+    isAdmin = serializers.SerializerMethodField(read_only = True)
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'name']
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin']
+        
+    def get__id(self,obj):
+        return obj.id
+    
+    
+    def get_isAdmin(self,obj):
+        return obj.is_staff
         
     def get_name(self,obj):
         name = obj.first_name 
