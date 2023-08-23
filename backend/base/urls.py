6262d4ca67b7de2a8405d261102363cfaf1220f0
Rokon Ron <rokon.raz@gmail.com>
2023-08-23 21:54:56 +0600

    JEAC

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,21 +1,21 @@
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
 
 
 
-urlpatterns = [
-    # path('', views.getRoutes, name="routes" ),
-    path('users/', views.getUsers, name="users" ),
+# urlpatterns = [
+#     # path('', views.getRoutes, name="routes" ),
+#     path('users/', views.getUsers, name="users" ),
     
-    path('products/', views.getProducts, name="products" ),
-    path('products/<str:pk>/', views.getProduct, name="product" ),
+#     path('products/', views.getProducts, name="products" ),
+#     path('products/<str:pk>/', views.getProduct, name="product" ),
     
-    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+#     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     
-    path('users/profile/', views.getUserProfile, name="user_profile" ),
+#     path('users/profile/', views.getUserProfile, name="user_profile" ),
     
-    path('users/register/', views.registerUser, name="register"),
+#     path('users/register/', views.registerUser, name="register"),
     
-]
+# ]
 
 
