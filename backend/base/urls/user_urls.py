328f480a5aca5956adb3a0480f540bd3a9b09299
Rokon Ron <rokon.raz@gmail.com>
2023-08-23 21:52:05 +0600

    Create user_urls.py file and update it.

---
+++ backend/base/urls/user_urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from base.views import user_urls as views
+
+
+urlpatterns = [
+    path('', views.getUsers, name="users" ),
+    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('profile/', views.getUserProfile, name="user_profile" ),
+    path('register/', views.registerUser, name="register"),
+]
+
+
