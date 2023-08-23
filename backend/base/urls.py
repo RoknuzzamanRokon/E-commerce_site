2fe1381755187c90ceb342dfd0820d5480736337
Rokon Ron <rokon.raz@gmail.com>
2023-08-23 13:27:04 +0600

    update views and urls for adding user views and connect urls in views.py file.

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes, name="routes" ),
+    path('users/', views.getUsers, name="users" ),
+    
     path('products/', views.getProducts, name="products" ),
     path('products/<str:pk>/', views.getProduct, name="product" ),
     
