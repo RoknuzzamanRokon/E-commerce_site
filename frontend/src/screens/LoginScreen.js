df8a017aa180d56506f52ddcbd694bd5cf686424
Rokon Ron <rokon.raz@gmail.com>
2023-08-24 12:59:01 +0600

    Create email and password field in loginscreen.

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -15,8 +15,9 @@ function LoginScreen() {
   return (
     <FormContainer>
         <h1>Sign In</h1>
-
         <Form onSubmit={submitHandler}>
+
+
           <Form.Group controlId='email'>
             <Form.Label>Email Address</Form.Label>
             <Form.Control
@@ -25,10 +26,24 @@ function LoginScreen() {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             >
-
             </Form.Control>
           </Form.Group>
 
+          <Form.Group controlId='password'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Enter password'
+              value={password}
+              onChange={(e) => setEmail(e.target.value)}
+            >
+            </Form.Control>
+          </Form.Group>
+
+          <Button type='submit' variant='primary'>
+            Sign In
+          </Button>
+          
         </Form>
     </FormContainer>
   )
