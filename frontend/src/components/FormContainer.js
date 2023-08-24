1e5cf9f132c0e12e743d6a95dfc8b18d450c9120
Rokon Ron <rokon.raz@gmail.com>
2023-08-24 12:18:27 +0600

    connet is other formconteiner and loginscreen.

---
+++ frontend/src/components/FormContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+function FormContainer() {
+  return (
+    <Container>
+        FormContainar
+    </Container>
+  )
+}
+
+export default FormContainer
--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Form, Button, Row, Col } from 'react-bootstrap'  
 import Loader from '../components/Loader'
 import Message from '../components/Message'
+import FormContainer from '../components/FormContainer'
 import { login } from '../actions/userActions'
 
 
@@ -12,9 +13,9 @@ function LoginScreen() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
   return (
-    <div>
+    <FormContainer>
         Login Screen
-    </div>
+    </FormContainer>
   )
 }
 
