6491c509acabce3186cd4da22401245feda1cdbf
Rokon Ron <rokon.raz@gmail.com>
2023-08-25 20:16:20 +0600

    update (user_urls.py) for create user_views for userprofile.

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.getUsers, name="users" ),
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('profile/', views.getUserProfile, name="user_profile" ),
+    path('profile/update/', views.updateUserProfile, name="update_user_profile" ),
     path('register/', views.registerUser, name="register"),
 ]
 
