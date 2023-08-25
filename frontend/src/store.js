e3621893430c4bcfa9927274dbf4505f500e916c
Rokon Ron <rokon.raz@gmail.com>
2023-08-25 11:58:36 +0600

    update (store.js) file for user register.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -26,7 +26,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
-import { userLoginReducer } from './reducers/userReducers'
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
 
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
@@ -49,6 +49,7 @@ const store = configureStore({
     productDetails: productDetailsReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
   },
   
     initialState,
