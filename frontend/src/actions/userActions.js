c5bff48f5e80fd29c8a67cc00837181c7cc4bdc0
Rokon Ron <rokon.raz@gmail.com>
2023-08-26 13:35:51 +0600

    update (profilescreen.js) in screen file for userUpdateProfile.and also User Details Bug Fix.

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -14,6 +14,7 @@ import {
     USER_DETAILS_REQUEST,
     USER_DETAILS_SUCCESS,
     USER_DETAILS_FAIL,
+    USER_DETAILS_RESET,
 
     
     USER_UPDATE_PROFILE_REQUEST,
@@ -58,6 +59,7 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
     localStorage.removeItem('userInfo')
     dispatch({ type: USER_LOGOUT})
+    dispatch({ type: USER_DETAILS_RESET })
 }
 
 
@@ -172,7 +174,7 @@ export const updateUserProfile = ( user ) => async (dispatch, getState ) => {
                     type: USER_LOGIN_SUCCESS,
                     payload: data
                 })
-                
+
                 localStorage.setItem('userInfo', JSON.stringify(data))
 
         } 
