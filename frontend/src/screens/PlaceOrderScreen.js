61fc169cb9d16f7f2a0d4e0a4ce7709bc855a020
Rokon Ron <rokon.raz@gmail.com>
2023-08-28 16:45:56 +0600

    update placeorderscreen and manage shipping,payment method,order items etc.and also create order summary.

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,13 +1,50 @@
-import React, { useState } from 'react'
-import { Form, Button, Row, Col, ListGroup } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import FormContainer from '../components/FormContainer'
+import Message from '../components/Message'
 import CheckoutStep from '../components/CheckoutStep'
-import { saveShippingAddress } from '../actions/cartActions'
-import { useNavigate  } from 'react-router-dom';    
+import { createOrder } from '../actions/orderActions'
+import { ORDER_CREATE_RESET } from '../constants/orderConstants'
+// import { useNavigate  } from 'react-router-dom';    
 
-function PlaceOrderScreen() {
+function PlaceOrderScreen({history}) {
+    // const orderCreate = useSelector(state => state.orderCreate)
+    // const { order, error, success } = orderCreate
+
+    // const dispatch = useDispatch()
+
     const cart = useSelector(state => state.cart)
+
+    cart.itemsPrice = cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0).toFixed(2)
+    cart.shippingPrice = (cart.itemsPrice > 100 ? 0 : 10).toFixed(2)
+    cart.taxPrice = Number((0.082) * cart.itemsPrice).toFixed(2)
+
+    cart.totalPrice = (Number(cart.itemsPrice) + Number(cart.shippingPrice) + Number(cart.taxPrice)).toFixed(2)
+
+
+    // if (!cart.paymentMethod) {
+    //     history.push('/payment')
+    // }
+
+    // useEffect(() => {
+    //     if (success) {
+    //         history.push(`/order/${order._id}`)
+    //         dispatch({ type: ORDER_CREATE_RESET })
+    //     }
+    // }, [success, history])
+
+    const placeOrder = () => {
+        // dispatch(createOrder({
+        //     orderItems: cart.cartItems,
+        //     shippingAddress: cart.shippingAddress,
+        //     paymentMethod: cart.paymentMethod,
+        //     itemsPrice: cart.itemsPrice,
+        //     shippingPrice: cart.shippingPrice,
+        //     taxPrice: cart.taxPrice,
+        //     totalPrice: cart.totalPrice,
+        // }))
+    }
 
 
 
@@ -39,12 +76,91 @@ function PlaceOrderScreen() {
                         </p>
                     </ListGroup.Item>
 
+                    <ListGroup.Item>
+                        <h2>Order Items</h2>
+                        {cart.cartItems.length===0 ? <Message variant='info'>
+                            Your cart is empty
+                        </Message> : (
+                            <ListGroup variant='flush'>
+                                {cart.cartItems.map((item, index) => (
+                                    <ListGroup.Item key={index}>
+                                        <Row>
+                                            <Col md={1}>
+                                                <Image src={item.image} alt={item.name} fluid rounded/>
+                                            </Col>
+
+                                            <Col>
+                                                <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                            </Col>
+
+                                            <Col md={4}>
+                                                {item.qty} X ${item.price} = ${(item.qty * item.price).toFixed(2)}
+                                            </Col>
+
+                                        </Row>
+                                    </ListGroup.Item>
+                                ))}
+                            </ListGroup>
+                        )}
+                    </ListGroup.Item>
+
                 </ListGroup>
             </Col>
 
             <Col md={4}>
-                something
-            </Col>
+                    <Card>
+                        <ListGroup variant='flush'>
+                            <ListGroup.Item>
+                                <h2>Order Summary</h2>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Items:</Col>
+                                    <Col>${cart.itemsPrice}</Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Shipping:</Col>
+                                    <Col>${cart.shippingPrice}</Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Tax:</Col>
+                                    <Col>${cart.taxPrice}</Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Total:</Col>
+                                    <Col>${cart.totalPrice}</Col>
+                                </Row>
+                            </ListGroup.Item>
+
+{/* 
+                            <ListGroup.Item>
+                                {error && <Message variant='danger'>{error}</Message>}
+                            </ListGroup.Item> */}
+
+                            <ListGroup.Item>
+                                <Button
+                                    type='button'
+                                    className='btn-block'
+                                    disabled={cart.cartItems === 0}
+                                    onClick={placeOrder}
+                                >
+                                    Place Order
+                                </Button>
+                            </ListGroup.Item>
+
+                        </ListGroup>
+                    </Card>
+                </Col>
         </Row>
     </div>
   )
