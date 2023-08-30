268474a7d2f85a579e7d266a1887ad75c7c1fc9c
Rokon Ron <rokon.raz@gmail.com>
2023-08-30 17:29:23 +0600

    update order_views input serializer.

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from base.serializer import ProductSerializer
+from base.serializer import ProductSerializer, OrderItemSerializer, OrderSerializer, ShippingAddressSerializer
 from base.models import Product, Order, OrderItem, ShippingAddress
 from base.products import products
 
@@ -57,6 +57,7 @@ def addOrderItem(request):
             # (4) update stock.
             product.countInStock -= item.qty
             product.save()
+    serializer = OrderSerializer(order, many=True)
         
     return Response('ORDER')
 
