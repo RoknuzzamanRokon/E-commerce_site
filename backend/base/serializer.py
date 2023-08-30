f0d53d1b3de0e278944cd8e90c7f01d0161ff6bc
Rokon Ron <rokon.raz@gmail.com>
2023-08-30 17:25:49 +0600

    update serializer for order_views

--- backend/base/serializer.py
+++ backend/base/serializer.py
@@ -57,6 +57,34 @@ class OrderItemSerializer(serializers.ModelSerializer):
         
 class ShippingAddressSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ShippingAddress
+        model = Product
         fields = '__all__'
+        
+        
+class OrderSerializer(serializers.ModelSerializer):
+    order = serializers.SerializerMethodField(read_only=True)
+    shippingAddress = serializers.SerializerMethodField(read_only=True)
+    user = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = Order
+        fields = '__all__'
+        
+    def get_orders(self, obj):
+        items = obj.orderitem_set.all()
+        serializer = OrderItemSerializer(items, many=True)
+        return serializer.data
+        
+    
+    def get_shippingAddress(self, obj):
+        try:
+            address = ShippingAddressSerializer(obj.shippingAddress, many=False)
+        except:
+            address = False
+        return address
+    
+    def get_orders(self, obj):
+        user = obj.user
+        serializer = UserSerializer (user, many=False)
+        return serializer.data
+        
         
